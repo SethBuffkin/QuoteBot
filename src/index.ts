2a81392d72a5f585ccaa20fd8e19a8eb90a88857
unfurledrelic <cmoughon@email.wm.edu>
2021-03-17 20:51:33 -0400

    Initial commit, sets up the project and a 'hello world' application

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+console.log("Hello world!!!!!!!");
+
+// notes from cmough/unfurledrelic:
+// this is a basic start for the quote bot
+// logic is likely as follows
+// while (1)
+//   listen in for commands
+//   if get command:
+//       do thing :)
